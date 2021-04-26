eaf3d8e52f6e696209696bb27882f3c510e01fc0
Atanusaha143 <atanu.saha415@gmail.com>
2021-04-27 02:49:08 +0600

    model implementation and inital migration

---
+++ TechShopCFAPI/TechShopCFAPI/Models/OldProduct.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TechShopCFAPI.Models
+{
+    public class OldProduct
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ProductName { get; set; }
+
+        [Required]
+        public string ProductDescription { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Status { get; set; }
+
+        [Required]
+        public decimal BuyingPrice { get; set; }
+
+        [Required]
+        public decimal SellingPrice { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Category { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Brand { get; set; }
+
+        public string Features { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Images { get; set; }
+
+        public Nullable<int> Discount { get; set; }
+
+        public Nullable<int> Tax { get; set; }
+    }
+}
---
+++ TechShopCFAPI/TechShopCFAPI/Models/TechShopDbContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TechShopCFAPI.Migrations;
+
+namespace TechShopCFAPI.Models
+{
+    public class TechShopDbContext : DbContext
+    {
+        public TechShopDbContext() : base("name=TechShopDbContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TechShopDbContext, Configuration>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //BuyingAgent
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.Id)
+                                              .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity)
+                                              .IsRequired();
+
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.FullName).HasColumnType("varchar");
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.UserName).HasColumnType("varchar");
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.ProfilePic).HasColumnType("varchar");
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.Password).HasColumnType("varchar");
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.Email).HasColumnType("varchar");
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.Phone).HasColumnType("varchar");
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.Address).HasColumnType("varchar");
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.Salary).HasColumnType("decimal").HasPrecision(18,1);
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.JoiningDate).HasColumnType("datetime");
+            modelBuilder.Entity<BuyingAgent>().Property(p => p.LastUpdated).HasColumnType("datetime");
+
+
+
+            //OldProduct
+            modelBuilder.Entity<OldProduct>().Property(p => p.Id)
+                                              .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity)
+                                              .IsRequired();
+            modelBuilder.Entity<OldProduct>().Property(p => p.CustomerId).HasColumnType("int");
+            modelBuilder.Entity<OldProduct>().Property(p => p.ProductName).HasColumnType("varchar");
+            modelBuilder.Entity<OldProduct>().Property(p => p.ProductDescription).HasColumnType("text");
+            modelBuilder.Entity<OldProduct>().Property(p => p.Status).HasColumnType("varchar");
+            modelBuilder.Entity<OldProduct>().Property(p => p.BuyingPrice).HasColumnType("decimal").HasPrecision(18, 1);
+            modelBuilder.Entity<OldProduct>().Property(p => p.SellingPrice).HasColumnType("decimal").HasPrecision(18, 1);
+            modelBuilder.Entity<OldProduct>().Property(p => p.Category).HasColumnType("varchar");
+            modelBuilder.Entity<OldProduct>().Property(p => p.Brand).HasColumnType("varchar");
+            modelBuilder.Entity<OldProduct>().Property(p => p.Features).HasColumnType("text");
+            modelBuilder.Entity<OldProduct>().Property(p => p.Quantity).HasColumnType("int");
+            modelBuilder.Entity<OldProduct>().Property(p => p.Images).HasColumnType("varchar");
+            modelBuilder.Entity<OldProduct>().Property(p => p.Discount).HasColumnType("int");
+            modelBuilder.Entity<OldProduct>().Property(p => p.Tax).HasColumnType("int");
+
+            //PurchaseLog
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.Id)
+                                              .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity)
+                                              .IsRequired();
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.CustomerId).HasColumnType("int");
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.ProductName).HasColumnType("varchar");
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.ProductDescription).HasColumnType("text");
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.Status).HasColumnType("varchar");
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.BuyingPrice).HasColumnType("decimal").HasPrecision(18, 1);
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.Category).HasColumnType("varchar");
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.Brand).HasColumnType("varchar");
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.Features).HasColumnType("text");
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.Quantity).HasColumnType("int");
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.Images).HasColumnType("varchar");
+            modelBuilder.Entity<PurchaseLog>().Property(p => p.PurchasedDate).HasColumnType("datetime");
+        }
+        public DbSet<BuyingAgent> BuyingAgents  { set; get; }
+        public DbSet<OldProduct> OldProducts { set; get; }
+        public DbSet<PurchaseLog> PurchaseLogs { set; get; }
+    }
+}
