dfe45f6a50feb8d972a98fe2e3828a244856103e
Atanusaha143 <atanu.saha415@gmail.com>
2021-04-28 03:31:24 +0600

    implemented repository pattern

---
+++ TechShopCFAPI/TechShopCFAPI/Repository/BuyingAgentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TechShopCFAPI.Models;
+
+namespace TechShopCFAPI.Repository
+{
+    public class BuyingAgentRepository : Repository<BuyingAgent>
+    {
+        TechShopDbContext context = new TechShopDbContext();
+
+        public bool BuyingAgnetLoginValidation(string email, string password)
+        {
+            return context.BuyingAgents.Any(x => x.Email.Equals(email) && x.Password.Equals(password));   
+        }
+
+        public BuyingAgent GetBuyingAgentByEmail(string email)
+        {
+            return GetAll().Where(x => x.Email.Equals(email)).FirstOrDefault();
+        }
+    }
+}
