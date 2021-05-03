eab08d974cd54ec0412ad3e398c2f7098f09849c
Atanusaha143 <atanu.saha415@gmail.com>
2021-05-04 01:08:27 +0600

    added necessary assets

--- Front-End/assets/JS/ViewPurchaseLog.js
+++ Front-End/assets/JS/ViewPurchaseLog.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
 
+	if(Cookies.get("Login") != 1)
+	{
+		window.location = "Login.html";
+	}
+
 		$.ajax({
 		url:"http://localhost:56213/api/purchase_log",
+		headers:{
+        	"Authorization":"basic "+Cookies.get("Authenticatior")
+    	},
 		complete:function(xmlHttp,status){
 			if(xmlHttp.status == 200)
 			{
-				var purchaseLog = "<table border='1' cellspacing='0'><tr><th>&nbsp Customer ID &nbsp</th><th>&nbsp Product Name &nbsp</th><th>&nbsp Product Description &nbsp</th><th>&nbsp Buying Price &nbsp</th><th>&nbsp Category &nbsp</th><th>&nbsp Brand &nbsp</th><th>&nbsp Features &nbsp</th><th>&nbsp Quantity &nbsp</th><th>&nbsp Purchased Date &nbsp</th></tr>";
+				var purchaseLog = "<table border='1' cellspacing='0'><tr><th>&nbsp Customer ID &nbsp</th><th>&nbsp Product Name &nbsp</th><th>&nbsp Product Description &nbsp</th><th>&nbsp Buying Price &nbsp</th><th>&nbsp Category &nbsp</th><th>&nbsp Brand &nbsp</th><th>&nbsp Features &nbsp</th><th>&nbsp Quantity &nbsp</th><th>&nbsp Image &nbsp</th><th>&nbsp Purchased Date &nbsp</th></tr>";
 				var purchaseLogData = xmlHttp.responseJSON;
 				console.log(purchaseLogData.length);
 				for (var i = 0; i < purchaseLogData.length; i++) {
-					 purchaseLog += "<tr><td>"+purchaseLogData[i].customerId+"</td><td>"+purchaseLogData[i].productName+"</td><td>"+purchaseLogData[i].productDescription+"</td><td>"+purchaseLogData[i].buyingPrice+"</td><td>"+purchaseLogData[i].category+"</td><td>"+purchaseLogData[i].brand+"</td><td>"+purchaseLogData[i].features+"</td><td>"+purchaseLogData[i].quantity+"</td><td>"+purchaseLogData[i].purchasedDate+"</td></tr>";
+					 purchaseLog += "<tr><td>"+purchaseLogData[i].customerId+"</td><td>"+purchaseLogData[i].productName+"</td><td>"+purchaseLogData[i].productDescription+"</td><td>"+purchaseLogData[i].buyingPrice+"</td><td>"+purchaseLogData[i].category+"</td><td>"+purchaseLogData[i].brand+"</td><td>"+purchaseLogData[i].features+"</td><td>"+purchaseLogData[i].quantity+"</td><td><img src='assets/img/Products/"+purchaseLogData[i].images+"' width='250' height='300'></td><td>"+purchaseLogData[i].purchasedDate+"</td></tr>";
 				}
 				purchaseLog += "</table>"
 				$("#showPurchaseLog").html(purchaseLog);
@@ -23,3 +31,126 @@ $(document).ready(function(){
 		});
 
 });
+
+
+
+function searchCat()
+{
+	var searchedElment = document.getElementById('searchBox').value.toLowerCase();
+	var flag = false;
+		$.ajax({
+		url:"http://localhost:56213/api/purchase_log",
+		headers:{
+        	"Authorization":"basic "+Cookies.get("Authenticatior")
+    	},
+		complete:function(xmlHttp,status){
+			if(xmlHttp.status == 200)
+			{
+				var purchaseLog = "<table border='1' cellspacing='0'><tr><th>&nbsp Customer ID &nbsp</th><th>&nbsp Product Name &nbsp</th><th>&nbsp Product Description &nbsp</th><th>&nbsp Buying Price &nbsp</th><th>&nbsp Category &nbsp</th><th>&nbsp Brand &nbsp</th><th>&nbsp Features &nbsp</th><th>&nbsp Quantity &nbsp</th><th>&nbsp Image &nbsp</th><th>&nbsp Purchased Date &nbsp</th></tr>";
+				var purchaseLogData = xmlHttp.responseJSON;
+				console.log(purchaseLogData.length);
+				for (var i = 0; i < purchaseLogData.length; i++) {
+					if(purchaseLogData[i].category.toLowerCase() == searchedElment)
+					 {
+					 	flag = true;
+					 	purchaseLog += "<tr><td>"+purchaseLogData[i].customerId+"</td><td>"+purchaseLogData[i].productName+"</td><td>"+purchaseLogData[i].productDescription+"</td><td>"+purchaseLogData[i].buyingPrice+"</td><td>"+purchaseLogData[i].category+"</td><td>"+purchaseLogData[i].brand+"</td><td>"+purchaseLogData[i].features+"</td><td>"+purchaseLogData[i].quantity+"</td><td><img src='assets/img/Products/"+purchaseLogData[i].images+"' width='250' height='300'></td><td>"+purchaseLogData[i].purchasedDate+"</td></tr>";
+					 }
+				}
+				purchaseLog += "</table>"
+				if(flag)
+				{
+					$("#showPurchaseLog").html(purchaseLog);
+				}
+				else
+				{
+					$("#msg").html("No matches!!");
+				}
+			}
+			else
+			{
+				$("#msg").html(xmlHttp.status+":"+xmlHttp.statusText);
+			}
+		}
+});
+}
+
+
+function searchByDate()
+{
+	var from = document.getElementById('from').value;
+	var to = document.getElementById('to').value;
+	let today = new Date().toISOString().slice(0, 10);
+
+	// console.log(from);
+	// console.log(to);
+	// console.log(today);
+
+	if(to > today || from > today)
+	{
+		document.getElementById("msg").innerHTML = "*Invalid date range";
+	}
+	else
+	{
+		$.ajax({
+		url:"http://localhost:56213/api/purchase_log",
+		headers:{
+        	"Authorization":"basic "+Cookies.get("Authenticatior")
+    	},
+		complete:function(xmlHttp,status){
+			if(xmlHttp.status == 200)
+			{
+				var purchaseLog = "<table border='1' cellspacing='0'><tr><th>&nbsp Customer ID &nbsp</th><th>&nbsp Product Name &nbsp</th><th>&nbsp Product Description &nbsp</th><th>&nbsp Buying Price &nbsp</th><th>&nbsp Category &nbsp</th><th>&nbsp Brand &nbsp</th><th>&nbsp Features &nbsp</th><th>&nbsp Quantity &nbsp</th><th>&nbsp Image &nbsp</th><th>&nbsp Purchased Date &nbsp</th></tr>";
+				var purchaseLogData = xmlHttp.responseJSON;
+				for (var i = 0; i < purchaseLogData.length; i++) {
+
+					var date = purchaseLogData[i].purchasedDate.slice(0,10);
+					console.log(date);
+					if(from <= date && date <= to)
+					{
+						purchaseLog += "<tr><td>"+purchaseLogData[i].customerId+"</td><td>"+purchaseLogData[i].productName+"</td><td>"+purchaseLogData[i].productDescription+"</td><td>"+purchaseLogData[i].buyingPrice+"</td><td>"+purchaseLogData[i].category+"</td><td>"+purchaseLogData[i].brand+"</td><td>"+purchaseLogData[i].features+"</td><td>"+purchaseLogData[i].quantity+"</td><td><img src='assets/img/Products/"+purchaseLogData[i].images+"' width='250' height='300'></td><td>"+purchaseLogData[i].purchasedDate+"</td></tr>";
+					}
+				}
+				purchaseLog += "</table>"
+				$("#showPurchaseLog").html(purchaseLog);
+			}
+			else
+			{
+				$("#msg").html(xmlHttp.status+":"+xmlHttp.statusText);
+			}
+		}
+
+		});
+	}
+
+
+}
+
+function sortPurchaseData()
+{
+	var sortBy = document.getElementById("select").value;
+	
+	$.ajax({
+	url:"http://localhost:56213/api/purchase_log/sortBy/"+sortBy,
+	headers:{
+    	"Authorization":"basic "+Cookies.get("Authenticatior")
+	},
+	complete:function(xmlHttp,status){
+		if(xmlHttp.status == 200)
+		{
+			var purchaseLog = "<table border='1' cellspacing='0'><tr><th>&nbsp Customer ID &nbsp</th><th>&nbsp Product Name &nbsp</th><th>&nbsp Product Description &nbsp</th><th>&nbsp Buying Price &nbsp</th><th>&nbsp Category &nbsp</th><th>&nbsp Brand &nbsp</th><th>&nbsp Features &nbsp</th><th>&nbsp Quantity &nbsp</th><th>&nbsp Image &nbsp</th><th>&nbsp Purchased Date &nbsp</th></tr>";
+			var purchaseLogData = xmlHttp.responseJSON;
+			console.log(purchaseLogData.length);
+			for (var i = 0; i < purchaseLogData.length; i++) {
+				 purchaseLog += "<tr><td>"+purchaseLogData[i].customerId+"</td><td>"+purchaseLogData[i].productName+"</td><td>"+purchaseLogData[i].productDescription+"</td><td>"+purchaseLogData[i].buyingPrice+"</td><td>"+purchaseLogData[i].category+"</td><td>"+purchaseLogData[i].brand+"</td><td>"+purchaseLogData[i].features+"</td><td>"+purchaseLogData[i].quantity+"</td><td><img src='assets/img/Products/"+purchaseLogData[i].images+"' width='250' height='300'></td><td>"+purchaseLogData[i].purchasedDate+"</td></tr>";
+			}
+			purchaseLog += "</table>"
+			$("#showPurchaseLog").html(purchaseLog);
+		}
+		else
+		{
+			$("#msg").html(xmlHttp.status+":"+xmlHttp.statusText);
+		}
+	}
+
+	});
+}
--- Front-End/assets/JS/script.js
+++ Front-End/assets/JS/script.js
@@ -1,11 +1,46 @@
 $(document).ready(function(){
 
+	if(Cookies.get("Login") != 1)
+	{
+		window.location = "Login.html";
+	}
+
+	var email = Cookies.get("Email");
 	$.ajax({
-		url:"http://localhost:56213/api/old_product",
+		url:"http://localhost:56213/api/buying_agent/",
+		method:"GET",
+		data:{
+			"email": email
+		},
+		headers:{
+            "Authorization":"basic "+Cookies.get("Authenticatior")
+        },
 		complete:function(xmlHttp,status){
 			if(xmlHttp.status == 200)
 			{
-				var oldProducts = "<table border='1' id='oldProductsList' cellspacing='0'><tr><th>Customer ID</th><th>Product Name</th><th>Product Description</th><th>Requested Price</th><th>Category</th><th>Brand</th><th>Features</th><th>Quantity</th><th>Options</th></tr>";
+				var buyingAgentData = xmlHttp.responseJSON;
+				Cookies.set("id",buyingAgentData.id);
+				console.log(buyingAgentData.id);
+			}
+			else
+			{
+				$("#msg").html("No Content");
+			}
+		}
+
+
+	});
+
+	$.ajax({
+		url:"http://localhost:56213/api/old_product",
+	    headers:{
+            "Authorization":"basic "+Cookies.get("Authenticatior")
+        },
+		complete:function(xmlHttp,status){
+			if(xmlHttp.status == 200)
+			{
+				var flag = false;
+				var oldProducts = "<table border='1' id='oldProductsList' cellspacing='0'><tr><th>Customer ID</th><th>Product Name</th><th>Product Description</th><th>Requested Price</th><th>Category</th><th>Brand</th><th>Features</th><th>Quantity</th><th>Image</th><th>Options</th></tr>";
 				var oldProductsData = xmlHttp.responseJSON;
 				var accept = "Accept";
 				var reject = "Reject";
@@ -13,16 +48,24 @@ $(document).ready(function(){
 				for (var i = 0; i < oldProductsData.length; i++) {
 					if(oldProductsData[i].status == "Pending")
 					 {
-					 	oldProducts += "<tr><td>"+oldProductsData[i].customerId+"</td><td>"+oldProductsData[i].productName+"</td><td>"+oldProductsData[i].productDescription+"</td><td>"+oldProductsData[i].buyingPrice+"</td><td>"+oldProductsData[i].category+"</td><td>"+oldProductsData[i].brand+"</td><td>"+oldProductsData[i].features+"</td><td>"+oldProductsData[i].quantity+"</td>";
-					 	oldProducts += "<td><a href='BuyingAgentHome.html?acceptId="+oldProductsData[i].id+"'class='linkBtn submitBtn'>"+accept+"</a><br><br><a href='BuyingAgentHome.html?rejectId="+oldProductsData[i].id+"'class='linkBtn logoutBtn''>"+reject+"</a></td><br></tr>";
+					 	flag = true;
+					 	oldProducts += "<tr><td align='center'><a href='BuyingAgentCustomer.html?customerId="+oldProductsData[i].customerId+"' class='linkBtn'>"+oldProductsData[i].customerId+"</a></td><td>"+oldProductsData[i].productName+"</td><td>"+oldProductsData[i].productDescription+"</td><td>"+oldProductsData[i].buyingPrice+"</td><td>"+oldProductsData[i].category+"</td><td>"+oldProductsData[i].brand+"</td><td>"+oldProductsData[i].features+"</td><td>"+oldProductsData[i].quantity+"</td><td><img src='assets/img/Products/"+oldProductsData[i].images+"'width='250' height='300'></td>";
+					 	oldProducts += "<td><a href='BuyingAgentHome.html?acceptId="+oldProductsData[i].id+"'class='linkBtn acceptBtn'>"+accept+"</a><br><br><a href='BuyingAgentHome.html?rejectId="+oldProductsData[i].id+"'class='linkBtn logoutBtn''>"+reject+"</a></td><br></tr>";
 					 }
 				}
 				oldProducts += "</table>"
-				$("#showOldProducts").html(oldProducts);
+				if(flag)
+				{
+					$("#showOldProducts").html(oldProducts);
+				}
+				else
+				{
+					$("#msg").html("Nothing to show!");
+				}
 			}
 			else
 			{
-				$("#msg").html(xmlHttp.status+":"+xmlHttp.statusText);
+				$("#msg").html("");
 			}
 		}
 
@@ -47,6 +90,9 @@ $(document).ready(function(){
 			url:"http://localhost:56213/api/old_product/accept/"+aId,
 			method:"POST",
 			headers:"Content-Type:application/json",
+			headers:{
+            	"Authorization":"basic "+Cookies.get("Authenticatior")
+        	},
 			complete:function(xmlHttp,status){
 				if(xmlHttp.status == 201)
 				{
@@ -67,6 +113,9 @@ $(document).ready(function(){
 			url:"http://localhost:56213/api/old_product/reject/"+rId,
 			method:"PUT",
 			headers:"Content-Type:application/json",
+			headers:{
+            	"Authorization":"basic "+Cookies.get("Authenticatior")
+        	},
 			complete:function(xmlHttp,status){
 				if(xmlHttp.status == 200)
 				{
@@ -83,3 +132,46 @@ $(document).ready(function(){
 	    }
 
 });
+
+function searchCategory()
+{
+	var searchedElment = document.getElementById('searchBoxHome').value.toLowerCase();
+	var flag = false;
+		$.ajax({
+		url:"http://localhost:56213/api/old_product",
+		headers:{
+            	"Authorization":"basic "+Cookies.get("Authenticatior")
+        },
+		complete:function(xmlHttp,status){
+			if(xmlHttp.status == 200)
+			{
+				var oldProducts = "<table border='1' id='oldProductsList' cellspacing='0'><tr><th>Customer ID</th><th>Product Name</th><th>Product Description</th><th>Requested Price</th><th>Category</th><th>Brand</th><th>Features</th><th>Quantity</th><th>Image</th><th>Options</th></tr>";
+				var oldProductsData = xmlHttp.responseJSON;
+				var accept = "Accept";
+				var reject = "Reject";
+				for (var i = 0; i < oldProductsData.length; i++) {
+					if(oldProductsData[i].category.toLowerCase() == searchedElment && oldProductsData[i].status == "Pending")
+					 {
+					 	flag = true;
+					 	oldProducts += "<tr><td>"+oldProductsData[i].customerId+"</td><td>"+oldProductsData[i].productName+"</td><td>"+oldProductsData[i].productDescription+"</td><td>"+oldProductsData[i].buyingPrice+"</td><td>"+oldProductsData[i].category+"</td><td>"+oldProductsData[i].brand+"</td><td>"+oldProductsData[i].features+"</td><td>"+oldProductsData[i].quantity+"</td>"+"</td><td><img src='assets/img/Products/"+oldProductsData[i].images+"'width='250' height='300'></td>";;
+					 	oldProducts += "<td><a href='BuyingAgentHome.html?acceptId="+oldProductsData[i].id+"'class='linkBtn acceptBtn'>"+accept+"</a><br><br><a href='BuyingAgentHome.html?rejectId="+oldProductsData[i].id+"'class='linkBtn logoutBtn''>"+reject+"</a></td><br></tr>";
+					 }
+				}
+				oldProducts += "</table>"
+				if(flag)
+				{
+					$("#showOldProducts").html(oldProducts);
+				}
+				else
+				{
+					$("#msg").html("No matches!!");
+				}
+			}
+			else
+			{
+				$("#msg").html(xmlHttp.status+":"+xmlHttp.statusText);
+			}
+		}
+
+});
+}
