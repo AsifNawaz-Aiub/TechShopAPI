eba6ceb2978e8d02521286c3d17e2e93882da167
Atanusaha143 <atanu.saha415@gmail.com>
2021-05-04 00:50:42 +0600

    added new functions

--- TechShopCFAPI/TechShopCFAPI/Repository/BuyingAgentRepository.cs
+++ TechShopCFAPI/TechShopCFAPI/Repository/BuyingAgentRepository.cs
@@ -8,13 +8,6 @@ namespace TechShopCFAPI.Repository
 {
     public class BuyingAgentRepository : Repository<BuyingAgent>
     {
-        TechShopDbContext context = new TechShopDbContext();
-
-        public bool BuyingAgnetLoginValidation(string email, string password)
-        {
-            return context.BuyingAgents.Any(x => x.Email.Equals(email) && x.Password.Equals(password));   
-        }
-
         public BuyingAgent GetBuyingAgentByEmail(string email)
         {
             return GetAll().Where(x => x.Email.Equals(email)).FirstOrDefault();
